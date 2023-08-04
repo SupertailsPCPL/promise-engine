134e82e3234fcb0f23eab5c06b461c8cadf735f5
Ajay N <119651259+ajaylovespets@users.noreply.github.com>
2023-08-04 09:53:05 +0530

    updates

--- controllers/Edd.controller.js
+++ controllers/Edd.controller.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const EddFunctions = require("./functions/edd_functions.js");
+const CartEddFunctions = require("./functions/eddcart_function.js");
 
 
 //METHOD: GET
@@ -8,7 +9,7 @@ const EddFunctions = require("./functions/edd_functions.js");
 //OUTPUT: RESPONSE
 //PATH = '/getEdd'
 
-router.get("/getEdd", async (req, res, next) => {
+router.get("/product/Edd", async (req, res, next) => {
    
     let cpin = req?.query?.cpin ?? false;
     let skus = req?.query?.skus ?? false;
@@ -16,6 +17,20 @@ router.get("/getEdd", async (req, res, next) => {
     console.log(cpin,skus,qty);
 
     let funcRes = await EddFunctions.EddMain(cpin,skus,qty);
+    return res.status(200).json({
+      message: "getEdd successfully returned",
+      response:cpin
+    }); 
+  });
+
+  router.get("/cart/Edd", async (req, res, next) => {
+   
+    let cpin = req?.query?.cpin ?? false;
+    let skus = req?.query?.skus ?? false;
+    let qty = req?.query?.qty ?? 1;
+    console.log(cpin,skus,qty);
+
+    let funcRes = await CartEddFunctions.EddMaincart(cpin,skus,qty);
     return res.status(200).json({
       message: "getEdd successfully returned",
       response:cpin
