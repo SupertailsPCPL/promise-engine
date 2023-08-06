d9abe3c786e7034458117783a758358e51f411d6
Ajay N <119651259+ajaylovespets@users.noreply.github.com>
2023-08-06 22:05:34 +0530

    updats on cronjob

--- cronjob.js
+++ cronjob.js
@@ -1,128 +1,150 @@
+// Import necessary modules
 const request = require('request');
 const connection = require('./dbpromiseEngine.js');
 
+// Function to fetch item master data from the database
+async function GetItemMaster() {
+    let promise = new Promise((resolve, reject) => {
+      try {
+        connection.query(
+          `Select * from EDDItemMaster`,
+          function (error, Logsresults, fields) {
+            if (error) {
+              console.log(error);
+              resolve(false);
+            } else {
+              resolve(JSON.parse(JSON.stringify(Logsresults)));
+            }
+          });
+      }
+      catch (e) {
+        console.log(e);
+        resolve(false);
+      }
+    });
+    return await promise;
+  }
 
-// const BATCH_SIZE = 1000; // Set the batch size according to your database limits
-
-const InventorySnapshot = async () => {
+// Function to fetch inventory data from UniCommerce API
+async function UniCommerceApiinventory(wareHouseId, skuid, accessToken) {
   try {
-   let itemMasterData = await GetItemMaster();
-//    console.log(itemMasterData);
-   let simpleItemMaster = [];
-   let bundleItemMaster = [];
-   for (let i = 0; i < itemMasterData.length; i++) {
-        if(itemMasterData[i].Type == "SIMPLE"){
-            simpleItemMaster.push(itemMasterData[i])
+    const options = {
+      url: 'https://warehousenow.unicommerce.com/services/rest/v1/inventory/inventorySnapshot/get',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Facility': wareHouseId,
+        'Authorization': `bearer ${accessToken}`
+      },
+      body: JSON.stringify({
+        "itemTypeSKUs": skuid
+      })
+    };
+
+    // Use a Promise to handle the API request and response
+    const response = await new Promise((resolve, reject) => {
+      request(options, function (error, response, body) {
+        if (error) {
+          console.error(error);
+          resolve(false);
+        } else {
+          if (!JSON.parse(body).length) {
+            resolve(false);
+          } else {
+            if (JSON.parse(body).successful === true) {
+              resolve(JSON.parse(body).inventorySnapshots);
+            } else {
+              resolve(false);
+            }
+          }
         }
-        else{
-            bundleItemMaster.push(itemMasterData[i])
-        }
-   }
-//    console.log(simpleItemMaster);
-   console.log(simpleItemMaster.length);
+      });
+    });
+
+    return response;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+// Function to perform bulk insertion of inventory data into the database
+async function bulkInsertInventory(inventoryData, wareHouseId) {
+  try {
+    if (!inventoryData || inventoryData.length === 0) {
+      console.log(`Nothing to insert for warehouse: ${wareHouseId}.`);
+      return;
+    }
+
+    // Prepare the data for bulk insertion
+    const values = inventoryData.map((inventory) => [
+      inventory.itemTypeSKU, // Replace with the appropriate SKU column name in your table
+      inventory.inventory, // Replace with the appropriate availableQuantity column name in your table
+    ]);
+
+    // The SQL query for bulk insertion with ON DUPLICATE KEY UPDATE
+    const sql = `
+      INSERT INTO promiseEngine.EdditemInventory (skuCode, \`${wareHouseId}\`)
+      VALUES ?
+      ON DUPLICATE KEY UPDATE \`${wareHouseId}\` = VALUES(\`${wareHouseId}\`)
+    `;
+
+    // Execute the bulk insertion query
+    await connection.query(sql, [values]);
+
+    console.log(`Bulk insertion completed for warehouse: ${wareHouseId}.`);
+  } catch (error) {
+    console.error(`Error during bulk insertion for warehouse: ${wareHouseId}:`, error);
+  }
+}
+
+// Function to process inventory data for a single warehouse
+async function processWarehouse(warehouseId) {
+  try {
+    // Fetch item master data only once to avoid unnecessary repetitions
+    const itemMasterData = await GetItemMaster();
+
+    // Filter the item master data to get simple items for this warehouse
+    const simpleItemMaster = itemMasterData.filter(item => item.Type === "SIMPLE");
+    const batchSize = 9000;
+
+    for (let i = 0; i < simpleItemMaster.length; i += batchSize) {
+      const batch = simpleItemMaster.slice(i, i + batchSize);
+      const skuIdsbatch = batch.flatMap(item => item?.skuId);
+
+      console.log(`Processing batch ${i} for warehouse: ${warehouseId}, batch size: ${batch.length}`);
+
+      // Fetch inventory data from UniCommerce API for the current batch and warehouse
+      const inventorySnapshots = await UniCommerceApiinventory(warehouseId, skuIdsbatch, "52bcbe95-07a2-4b9f-9863-a37105a14acc");
+
+      // Insert or update the inventory data into the database for the current warehouse
+      await bulkInsertInventory(inventorySnapshots, warehouseId);
+    }
   } catch (error) {
     console.error('Error:', error);
   }
-};
-
-// const bulkInsertToTable = async (data) => {
-//   try {
-//     // Split the data into batches
-//     const batches = [];
-//     for (let i = 0; i < data.length; i += BATCH_SIZE) {
-//       batches.push(data.slice(i, i + BATCH_SIZE));
-//     }
-
-//     for (const batch of batches) {
-//       // Prepare the bulk insert query
-//       let query = 'INSERT INTO promiseEngine.EDDItemMaster(skuId, weight, Type, componentSkusData) VALUES ';
-//       const values = batch.map((item) => {
-//         return `('${item.skuId}', '${item.weight}', '${item.Type}', '${JSON.stringify(item.componentSkusData)}')`;
-//       });
-
-//       query += values.join(',');
-
-//       // Execute the bulk insert query
-//       await connection.query(query);
-//     }
-//   } catch (error) {
-//     console.error('Error:', error);
-//   }
-// };
-
-InventorySnapshot();
-
-
-
-
-
-async function GetItemMaster(orderId) {
-    let promise = new Promise((resolve, reject) => {
-        try {
-            connection.query(
-                `Select * from EDDItemMaster`,
-                function (error, Logsresults, fields) {
-                    if (error) { console.log(error); }
-                    if (Logsresults) {
-                        resolve(JSON.parse(JSON.stringify(Logsresults)));
-                    } else {
-                        resolve(false);
-                    }
-                });
-        }
-        catch (e) {
-            console.log(e);
-            resolve(false);
-        }
-    });
-    return await promise
 }
 
-
-
-//get inventory from warehouseID,skuid and acessToken from uni commerce api
-// UniCommerceApiinventory("WN-MBLR0001",["CFOWF0003MO","DTRJT0068JH"],"4a2767fb-1e7c-4ebc-9665-deb7754d4599")
-async function UniCommerceApiinventory(wareHouseId, skuid, accessToken) {
-
-    let promise = new Promise(async (resolve, reject) => {
-
-        try {
-            const options = {
-                url: 'https://warehousenow.unicommerce.com/services/rest/v1/inventory/inventorySnapshot/get',
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Facility': wareHouseId,
-                    'Authorization': `bearer ${accessToken}`
-                },
-                body: JSON.stringify({
-                    "itemTypeSKUs": skuid
-                })
-            };
-            await request(options, function (error, response, body) {
-                if (error) {
-                    console.error(error);
-                    resolve(false);
-                } else {
-                    if (!JSON.parse(JSON.stringify(body)).length) {
-                        resolve(false);
-                    }
-                    else {
-                        if (JSON.parse(body).successful === true) {
-                            console.log(JSON.parse(body).inventorySnapshots);                            
-                            resolve(JSON.parse(body).inventorySnapshots);
-                        }
-                        else {
-                            resolve(false);
-                        }
-                    }
-                }
-            });
-        }
-        catch (e) {
-            console.error(e);
-            resolve(false);
-        }
-    });
-    return await promise;
+// Function to run the inventory snapshot for multiple warehouses concurrently
+async function runInventorySnapshotForWarehouses(warehouseIds) {
+  try {
+    // Use Promise.all to run the processWarehouse function for each warehouse concurrently
+    await Promise.all(warehouseIds.map(warehouseId => processWarehouse(warehouseId)));
+    return true; // Return true after all warehouses have been processed successfully
+  } catch (error) {
+    console.error('Error:', error);
+    return false; // Return false if there is any error during the process
+  }
 }
+
+// List of warehouses to process inventory data
+const warehouses = ["WH004", "WH005", "PWH001", "WN-MDEL0002", "WN-MBHI0003", "WN-MBLR0001"];
+
+// Start the inventory snapshot process for all warehouses and return the result
+runInventorySnapshotForWarehouses(warehouses)
+  .then(result => {
+    console.log('Inventory snapshot process completed:', result);
+  })
+  .catch(err => {
+    console.error('Error during inventory snapshot process:', err);
+  });
