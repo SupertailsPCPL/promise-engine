bdff0f3bb87b87170f6ed824ba8eac80bc871e2b
harsha <harsha.p@supertails.com>
2023-08-09 00:17:15 +0530

    updates

--- controllers/script/bulkeddscript.js
+++ controllers/script/bulkeddscript.js
@@ -1,11 +1,11 @@
 const XLSX = require('xlsx');
 const CartEddFunctions = require("../functions/eddcart_function");
-const workbook = XLSX.readFile('./');
+const workbook = XLSX.readFile('controllers/script/eddData.xlsx');
 const fs = require('fs');
 
 
 // file for where you want to store the response
-const filePath = 'response.txt';
+const filePath = 'controllers/script/response.txt';
 
 // Choose the sheet you want to read from (e.g., the first sheet)
 const sheetName = workbook.SheetNames[0];
@@ -13,14 +13,14 @@ const sheet = workbook.Sheets[sheetName];
 
 // Define the range of cells containing the data
 const range = XLSX.utils.decode_range(sheet['!ref']);
-const startRow = range.s.r + 2; // Skip the header row
-const endRow = range.e.r + 1;
+const startRow = range.s.r + 1; // Skip the header row
+const endRow = range.e.r ;
 
 // Loop through the rows and extract the phone number, name, and type
 for (let row = startRow; row <= endRow; row++) {
-    const cpinCell = sheet[`A${row}`];
-    const skusCell = sheet[`B${row}`];
-    const qtyCell = sheet[`C${row}`];
+    const cpinCell = sheet[XLSX.utils.encode_cell({ r: row, c: 0 })];
+    const skusCell = sheet[XLSX.utils.encode_cell({ r: row, c: 1 })];
+    const qtyCell = sheet[XLSX.utils.encode_cell({ r: row, c: 2 })];
 
     const cpin = cpinCell.v;
     const skus = skusCell.v;
@@ -30,10 +30,16 @@ for (let row = startRow; row <= endRow; row++) {
     console.log('Skus:', skus);
     console.log('Qty:', qty);
     try {
-        let response = CartEddFunctions.EddMaincart(cpin, skus, qty);
-        fs.appendFile(filePath, `${JSON.stringify(response)}\n`, (err) => { console.log(err) });
+        CartEddFunctions.EddMaincart(cpin, skus, qty)
+        .then(response => {
+            fs.appendFile(filePath, `${JSON.stringify(response)} \n`, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        })
     } catch (error) {
-        console.log("error");
-        console.log(error)
+        console.log("Error:", error);
     }
+
 }
