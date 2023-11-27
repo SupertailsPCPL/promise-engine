f1be3050c15ce1b551e793b19d1a3e4413d1ecdf
Ajay N <119651259+ajaylovespets@users.noreply.github.com>
2023-11-27 21:55:34 +0000

    updates

--- cronjob/cronjob.controller.js
+++ cronjob/cronjob.controller.js
@@ -19,6 +19,10 @@ router.get("/inventory", async (req, res, next) => {
   console.log("bunde cron ended",Date());  
     return res.status(200).json({
       message: "itemInv cronjob successfully completed",
+      bundleInv:bundleInv,
+      singleInv:funcRes
+
+
     }); 
   });
 
